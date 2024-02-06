9b1a4d9cf81a9ce753835f7f36c82a46b5339e9a
onwegabriel <gabrielonwe9@gmail.com>
2024-02-06 16:26:03 +0100

    first commit

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi} from 'openai';
-// import { OpenAIApi } from 'openai';
+import Configuration from 'openai';
+import OpenAIApi from 'openai';
 
 
 dotenv.config();
